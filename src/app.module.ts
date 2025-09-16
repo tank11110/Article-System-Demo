8cedbd9d42e47abbc7cd1c52dcb4ced3d2a292a5
otakuyoung <tank110101313@gmail.com>
2025-09-16 08:16:04 +0800

    feat(articles): scaffold resource (controller, service, module, dto)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
-  imports: [],
+  imports: [ArticlesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
