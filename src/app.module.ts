c3006952502c1bff4f034f7b1c52d26d07c9644d
otakuyoung <tank110101313@gmail.com>
2025-09-16 10:07:16 +0800

    feat(comments): scaffold resource (controller, service, module, dto)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { ArticlesModule } from './articles/articles.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [ArticlesModule],
+  imports: [ArticlesModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
